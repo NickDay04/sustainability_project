27cddc6e3c0d88431addd1a0ccee99afe3e33070
Nicholas Day <nicholas.day2004.nd@gmail.com>
2025-03-28 19:39:22 +0000

    Added .gitignore

--- build_db.sh
+++ build_db.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "[INFO] Killing old database process"
-docker kill alder_db
+docker kill sustain_proj
 echo "[INFO] Removing old database image"
-docker rm alder_db
+docker rm sustain_proj
 echo "[INFO] Pulling latest MySQL version"
 docker pull mysql/mysql-server:latest
 echo "[INFO] Running docker image"
-docker run --name alder_db -p 3306:3306 -e MYSQL_ROOT_HOST=% -e MYSQL_ROOT_PASSWORD=root -e MYSQL_USER=root -d mysql/mysql-server
+docker run --name sustain_proj -p 3306:3306 -e MYSQL_ROOT_HOST=% -e MYSQL_ROOT_PASSWORD=root -e MYSQL_USER=root -d mysql/mysql-server
